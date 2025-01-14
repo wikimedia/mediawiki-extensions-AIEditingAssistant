bbeb7916fdfb723e6e5e16a097bec8b58e8be44b
hslater <hamish.slater@hallowelt.com>
2025-01-14 13:49:51 +0100

    Use body parameters instead of post parameters
    Bug: T365757
    
    [REL1_43, master]
    
    Change-Id: I1b45ce2ac8908492f113cc05fe8838834a3f6369

--- src/Rest/PromptHandler.php
+++ src/Rest/PromptHandler.php
@@ -5,8 +5,6 @@ namespace MediaWiki\Extension\AIEditingAssistant\Rest;
 use MediaWiki\Extension\AIEditingAssistant\ProviderFactory;
 use MediaWiki\Rest\HttpException;
 use MediaWiki\Rest\SimpleHandler;
-use MediaWiki\Rest\Validator\BodyValidator;
-use MediaWiki\Rest\Validator\JsonBodyValidator;
 use Wikimedia\ParamValidator\ParamValidator;
 
 class PromptHandler extends SimpleHandler {
@@ -44,27 +42,23 @@ class PromptHandler extends SimpleHandler {
 		}
 	}
 
-	/**
-	 * @param string $contentType
-	 *
-	 * @return BodyValidator
-	 */
-	public function getBodyValidator( $contentType ) {
-		if ( $contentType === 'application/json' ) {
-			return new JsonBodyValidator( [
-				'command' => [
-					ParamValidator::PARAM_REQUIRED => true,
-				],
-				'text' => [
-					ParamValidator::PARAM_REQUIRED => true,
-				],
-				'isContinuation' => [
-					ParamValidator::PARAM_TYPE => 'boolean',
-					ParamValidator::PARAM_DEFAULT => false,
-				],
-			] );
-		}
-		return parent::getBodyValidator( $contentType );
+	/** @inheritDoc */
+	public function getBodyParamSettings(): array {
+		return [
+			'command' => [
+				self::PARAM_SOURCE => 'body',
+				ParamValidator::PARAM_REQUIRED => true,
+			],
+			'text' => [
+				self::PARAM_SOURCE => 'body',
+				ParamValidator::PARAM_REQUIRED => true,
+			],
+			'isContinuation' => [
+				self::PARAM_SOURCE => 'body',
+				ParamValidator::PARAM_TYPE => 'boolean',
+				ParamValidator::PARAM_DEFAULT => false,
+			],
+		];
 	}
 
 	/**
