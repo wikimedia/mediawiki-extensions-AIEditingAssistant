84fbc2b58ee2ed1736eacdfcc7b8a03b6d42d279
hslater <hamish.slater@hallowelt.com>
2025-01-23 12:18:11 +0100

    Update Config namespace
    ERM38444
    
    [REL1_43, master]

--- src/ConfigDefinition/ProviderType.php
+++ src/ConfigDefinition/ProviderType.php
@@ -4,10 +4,10 @@ namespace MediaWiki\Extension\AIEditingAssistant\ConfigDefinition;
 
 use BlueSpice\ConfigDefinition\ArraySetting;
 use BlueSpice\ConfigDefinition\IOverwriteGlobal;
-use Config;
 use HTMLFormField;
 use HTMLSelectField;
 use IContextSource;
+use MediaWiki\Config\Config;
 use MediaWiki\Extension\AIEditingAssistant\ProviderFactory;
 use MediaWiki\MediaWikiServices;
 
